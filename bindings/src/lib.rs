937db4f66e5801e967b15fb68e08ec9e0aef4552
Bhargav Kumar Nath <bhargavkumarnathh@gmail.com>
2025-10-24 15:34:25 +0100

    GitHub Actions

--- bindings/src/lib.rs
+++ bindings/src/lib.rs
@@ -1,14 +1,16 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use pyo3::prelude::*;
+use pyo3::wrap_pyfunction;
+
+/// Simple function to test the FFI bridge
+#[pyfunction]
+fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+    Ok((a + b).to_string())
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+/// A Python module implemented in Rust
+#[pymodule]
+fn advanced_ds_playground(_py: Python, m: &PyModule) -> PyResult<()> {
+    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    Ok(())
 }
+
